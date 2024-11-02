00398a78f7b6eedab6ebae37050bc4fd1816e741
bchan18 <114227244+bchan18@users.noreply.github.com>
2024-11-02 02:07:25 -0700

    Added Main menu

---
+++ main.py
@@ -0,0 +1,21 @@
+import csv
+
+
+def main():
+    print("==================================================================="
+          "===========================")
+    print("Music Application")
+    print("\nYou can search for songs and artists and create playlists to keep"
+          "track of your favorite songs")
+    print("==================================================================="
+          "============================")
+    print("\n\nPlease select an option:")
+    print("1. Find Artist")
+    print("2. Find Songs")
+    print("3. Add Song to Playlist")
+    print("4. View Playlist")
+    input("\nEnter an option: ")
+
+
+if __name__ == "__main__":
+    main()
